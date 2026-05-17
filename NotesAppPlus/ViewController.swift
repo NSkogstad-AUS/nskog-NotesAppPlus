d12c415e3f1ff3406c01557aad0ea5987a38c3dd
NSkogstad-AUS <nskogstad@student.unimelb.edu.au>
2026-05-17 18:17:45 +1000

    Initial Commit

---
+++ NotesAppPlus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NotesAppPlus
+//
+//  Created by Nicolai Skogstad on 17/5/2026.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ NotesAppPlusTests/NotesAppPlusTests.swift
@@ -0,0 +1,19 @@
+//
+//  NotesAppPlusTests.swift
+//  NotesAppPlusTests
+//
+//  Created by Nicolai Skogstad on 17/5/2026.
+//
+
+import Testing
+@testable import NotesAppPlus
+
+struct NotesAppPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
